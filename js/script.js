e9fccd81b97c7846ead3477e5854cb68b27ef00e
Brianna Gonzalez <bgonzalez2019@brooklynprephs.org>
2019-04-03 21:42:59 +0000

    project

--- js/script.js
+++ js/script.js
@@ -4,7 +4,22 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  //https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC
+// #1 get the user's search term
+var searchTerm =  $("#search-term").val();
+  // #2 make the actual ajax request call
+  $.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC" ,
+    method: "GET",
+    success: function(response) {
+        
+        console.log(response);
+        var gifURL  = '<img src="' + response.data[0].images.original.url+ '"/>';
+        //var gifURL = "fake";
+        $("#container").html(gifURL);
+    },
+    });
+  // #3 in the success function of the call, append the data to the html so that the giphys show up on the
   
   
 });
